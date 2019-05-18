21678f370f59fa1ef9fbf61adc98ffae21ff4837
andser91 <andreaserrecchia@hotmail.it>
2019-05-18 18:03:35 +0200

    sistemato dataset

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ x_training = []
 x_test = []
 y_training = []
 y_test = []
-with open("lang_file.json") as json_file:
+with open("lang_file.json",encoding="utf8") as json_file:
     data = json.load(json_file)
 
 print(data["it"])
@@ -26,9 +26,17 @@ print("==================== Download Pages =====================")
 for site in sites:
     try:
         i = i+1
-        page = requests.get(site)
-        html_code = page.content
-        soup = BeautifulSoup(html_code, 'html.parser')
+        if "amazon" in site:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+            page = requests.get(site, headers=headers)
+            soup = BeautifulSoup(page.content, 'lxml')
+
+        else:
+            page = requests.get(site)
+            html_code = page.content
+            soup = BeautifulSoup(html_code, 'html.parser')
+
         print("----------------- "+ site + " -------------")
         # kill all script and style elements
         for script in soup(["script", "style"]):
@@ -48,49 +56,49 @@ for site in sites:
         if cart == []:
             cart = soup.select('*[id*=cart], *[id*=Cart],*[id*=basket], *[id*=Basket], *[class*=cart], *[class*=Cart], *[class*=basket], *[class*=Basket]')
 
-        if cart != [] and label[sites.index(site)] == "1":
+        if (cart != [] and label[sites.index(site)] == "1") or (cart == [] and label[sites.index(site)] == "0"):
             j = j + 1
-            print("Ho trovato il carrello e la label è 1")
+            #print("Ho trovato il carrello e la label è 1")
             print(j)
 
-        trovato = False
-        if cart == []:
-            footer = soup.select('*[id*=footer], *[id*=Footer]')
-            if footer == []:
-                footer = soup.select('*[class*=Footer], *[class*=footer]')
-                if footer == []:
-                    footer = soup.find("footer")
-            if footer == None:
-                trovato = False
-            else:
-                footertext = []
-                for el in footer:
-                    footertext.append(el.text)
-                # elimino punteggiatura
-                footertext = elaborazione_testo.elimina_punteggiatura(footertext)
-                # tokenizzazione
-                footertext = elaborazione_testo.tokenize(footertext)
-                # trasformo le parole in lettere miniscole
-                footertext = elaborazione_testo.trasforma_in_minuscolo(footertext)
-                for key in dizionarioFooter:
-                    for el in footertext:
-                        if dizionarioFooter[key].get("en") in el:
-                            trovato = True
-                            print(dizionarioFooter[key].get(language))
-                        elif dizionarioFooter[key].get(language) in el:
-                            print(dizionarioFooter[key].get("en"))
-                            trovato = True
 
-
-            if trovato == True and label[sites.index(site)] == "1":
-                j = j + 1
-                print("ho trovato una parola chiave nel footer e la label è 1")
-                print(j)
-
-            if trovato == False and label[sites.index(site)] == "0":
-                j = j + 1
-                print("non ho trovato nè carrello nè una parola chiave nel footer e la label è 0")
-                print(j)
+        # trovato = False
+        # if cart == []:
+        #     footer = soup.select('*[id*=footer], *[id*=Footer]')
+        #     if footer == []:
+        #         footer = soup.select('*[class*=Footer], *[class*=footer]')
+        #         if footer == []:
+        #             footer = soup.find("footer")
+        #     if footer == None:
+        #         trovato = False
+        #     else:
+        #         footertext = []
+        #         for el in footer:
+        #             footertext.append(el.text)
+        #         # elimino punteggiatura
+        #         footertext = elaborazione_testo.elimina_punteggiatura(footertext)
+        #         # tokenizzazione
+        #         footertext = elaborazione_testo.tokenize(footertext)
+        #         # trasformo le parole in lettere miniscole
+        #         footertext = elaborazione_testo.trasforma_in_minuscolo(footertext)
+        #         for key in dizionarioFooter:
+        #             for el in footertext:
+        #                 if dizionarioFooter[key].get("en") in el:
+        #                     print(dizionarioFooter[key].get("en"))
+        #                     trovato = True
+        #                 elif dizionarioFooter[key].get(language) in el:
+        #                     trovato = True
+        #                     print(dizionarioFooter[key].get(language))
+        #
+        #     if trovato == True and label[sites.index(site)] == "1":
+        #         j = j + 1
+        #         print("ho trovato una parola chiave nel footer e la label è 1")
+        #         print(j)
+        #
+        #     if trovato == False and label[sites.index(site)] == "0":
+        #         j = j + 1
+        #         print("non ho trovato nè carrello nè una parola chiave nel footer e la label è 0")
+        #         print(j)
     except Exception as e:
         print(e)
 print(j)
